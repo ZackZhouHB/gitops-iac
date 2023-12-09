faad050a886c7961c9c21a51dea2538d0c2ead1f
Zackz <zhbsoftboy1@gmail.com>
2023-12-10 10:51:02 +1100

    modify node group

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -17,22 +17,13 @@ module "eks" {
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
-      instance_types = ["t2.medium"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-
-    two = {
-      name = "node-group-2"
-
+      capacity_type = "SPOT"
       instance_types = ["t3.medium"]
 
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
     }
+
   }
 }
